c0ed18196c89f3c04482c9a48e489449fec3a571
AntociM <gidioimihaela@gmail.com>
2022-04-02 19:36:45 +0000

    Add style.css

--- store/views.py
+++ store/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    """" A viw to retun the index page """
+    """" A view to retun the index page """
     
     return render(request, 'store/index.html')
