fec13c2104f7d686a0022751a932c10961074c58
AntociM <gidioimihaela@gmail.com>
2022-04-11 17:26:17 +0000

    Add products fixtures

---
+++ store/fixtures/generate-products-fixture.py
@@ -0,0 +1,33 @@
+import json
+
+f = open('products-database.json')
+data = json.load(f)
+data_filtered = []
+
+
+for product in data:
+    # Create product dict
+    product_dict = {}
+    fields = {}
+    
+    fields['name'] = product['name']
+    fields['description'] = product['description']
+    fields['brand'] = product['brand']
+    fields['price'] = product['price']
+    fields['rating'] = product['rating']
+    fields['image'] = product['image']
+    fields['category'] = product['categories'][0]
+
+    product_dict['model'] = 'store.Item'
+    product_dict['pk'] = product['objectID']
+    product_dict['fields'] = fields
+
+    data_filtered.append(product_dict)
+
+with open('products.json', 'w') as outfile:
+    json.dump(data_filtered, outfile)
+
+
+    
+
+
