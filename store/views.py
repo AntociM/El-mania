becd733c866d4299eca385369b346f7360893832
AntociM <gidioimihaela@gmail.com>
2022-04-12 18:43:15 +0000

    Render offers in index.html cards

--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import ItemDiscount
 
 # Create your views here.
 
 def index(request):
-    """" A view to retun the index page """
     
-    return render(request, 'store/index.html')
+    """" A view to retun the index page """
+    offers = ItemDiscount.objects.all()
+    context = {'offers': offers}
+    return render(request, 'store/index.html', context)
