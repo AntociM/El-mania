54243ee786021ce6d0fc4952aa51024f9c1868f9
AntociM <gidioimihaela@gmail.com>
2022-04-15 15:20:13 +0000

    Display of the discounted procent and prices

--- store/views.py
+++ store/views.py
@@ -7,5 +7,22 @@ def index(request):
     
     """" A view to retun the index page """
     offers = ItemDiscount.objects.all()
-    context = {'offers': offers}
+    discounted_items = []
+    for offer in offers:
+        offer_dict = {
+             "image": offer.item.image,
+             "name": offer.item.name,
+             "price": offer.item.price,
+             "discount": offer.discount_procent,
+             "new_price": offer.item.price - (offer.discount_procent * offer.item.price) / 100,
+        }
+        discounted_items.append(offer_dict)
+    
+    context = {
+        # 'offers': offers,
+        'discounted_items': discounted_items,
+    }
+ 
     return render(request, 'store/index.html', context)
+
+    
