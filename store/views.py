ebf6875cffb30f1e79879147e066d3b627b29aa8
AntociM <gidioimihaela@gmail.com>
2022-04-18 18:34:56 +0000

    Add buttons for product-detail page

--- store/views.py
+++ store/views.py
@@ -20,6 +20,7 @@ def index(request):
              "discount": offer.discount_procent,
             #  "new_price": round(offer.item.price - (offer.discount_procent * offer.item.price) / 100,2),
              "new_price": offer.new_price,
+             "item_id":  offer.item.pk
         }
         discounted_items.append(offer_dict)
     
