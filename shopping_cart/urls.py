df4546adf9e65368b4796dfbec67ad8e910ba47d
AntociM <gidioimihaela@gmail.com>
2022-04-20 20:01:51 +0000

    Add add_to_cart view and connect it in the URL

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shopping_cart_view, name='shopping_cart_view')
+    path('', views.shopping_cart_view, name='shopping_cart_view'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart')
 ]
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Item
+from .models import CartItem
+from datetime import datetime
 
 # Create your views here.
 
 def shopping_cart_view(request):
     """ A view that renders the shopping cart contents page """
-
+    
     return render(request, 'shopping_cart/shopping_cart.html')
+
+
+def add_to_cart(request, item_id):
+    # Retrive product
+    product = get_object_or_404(Item, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+
+    if not request.user.is_authenticated:
+        print("User is not authenticated")
+
+    try:
+        # If the user has the product in the bag, increase the quantity
+        current_items = CartItem.objects.get(user_id=request.user.pk, item=product)
+        current_items.quantity += 1
+        current_items.save()
+    except ObjectDoesNotExist:
+        # Create cart item
+        cart_item = CartItem()
+        cart_item.item = product
+        cart_item.user_id = request.user.pk
+        cart_item.quantity = 1
+        cart_item.total = 0
+        cart_item.created_at = datetime.now()
+        cart_item.modified_at = datetime.now()
+        cart_item.save()
+
+    return redirect(redirect_url)
