89570446cfd5e229595062cd22fe52fd784d3e11
AntociM <gidioimihaela@gmail.com>
2022-04-23 11:51:11 +0000

    Customize the OrderForm

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,4 +7,26 @@ class OrderForm(forms.ModelForm):
         fields = (('full_name', 'email', 'phone_number',
                   'address', 'city', 'postcode'
                   'county', 'country', ))
-                 
+    
+     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'city': 'Town or City',
+            'address': " Street address"
+            'county': 'County',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            # self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
