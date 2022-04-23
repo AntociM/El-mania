d89f1142801628441baf66768c929c56070eead8
AntociM <gidioimihaela@gmail.com>
2022-04-23 16:01:02 +0000

    Add core structure for checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
+from shopping_cart.models import CartItem
 from .forms import OrderForm
 
 # Create your views here.
 
 def checkout(request):
-    cart = request.session.get('cart', {})
-    if not cart:
-        message.error(request, "There's nothing in your cart")
+    cart_items = CartItem.objects.filter(user_id=request.user.pk)
+    if not cart_items:
+        messages.error(request, "There's nothing in your cart")
         return redirect(reverse('products'))
 
-        order_form = OrderForm()
-        context = { 
-            'order_form': order_form,
-        }
+    order_form = OrderForm()
+    context = { 
+        'order_form': order_form,
+        'cart_items': cart_items,
+    }
 
-        return render(request, 'checkout/checkout.html', context)
+    return render(request, 'checkout/checkout.html', context)
