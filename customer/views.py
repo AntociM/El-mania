f4ceec79335666cedab3bbb127e507f522927393
AntociM <gidioimihaela@gmail.com>
2022-04-26 21:20:52 +0000

    Add profie view, template and urls

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile(request):
+    """ Display of user's profile """
+    template = "customer/profile.html"
+    context = {}
+
+    return render(request, template, context)
