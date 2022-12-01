435edf736395597c574c5e47a998c60a2bac3442
AntociM <89809932+AntociM@users.noreply.github.com>
2022-12-01 08:17:00 +0100

    Prepare Render deployment

--- el_mania/settings.py
+++ el_mania/settings.py
@@ -34,6 +34,9 @@ else:
     DEBUG = True
 
 ALLOWED_HOSTS = ["el-mania.herokuapp.com", "localhost"]
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
